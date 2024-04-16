5da0ebe680ba983e7bfa9e33c41e38f5569824b3
Zryderace <147004178+Zryderace@users.noreply.github.com>
2024-04-16 16:52:28 +0200

    fibonacci

---
+++ PracticaAlgoritmos/src/Util/Algoritmos.java
@@ -0,0 +1,36 @@
+package Util;
+
+public abstract class Algoritmos {
+
+	/**
+	 * Devuelve el numero Fibonacci de la posicion dada, int
+	 * 
+	 * @param numero del que queremos calcular su posicion en la sucesion Fibonacci
+	 * @return entero existente en la posicion Fibonacci del numero dado
+	 */
+	
+	public static int fibonacci(int numero) {
+		
+		int aux0 = 0;
+		int aux1 = 1;
+		int aux = 0;
+		
+		if(numero==0) {
+			return 0;
+		} else if (numero==1) {
+			return 1;
+		} else {
+			
+			for (int i = 0; i < numero; i++) {
+				aux = aux0 + aux1;
+				aux0 = aux1;
+				aux1 = aux;
+			}
+			
+		}
+		return aux;
+	}
+	
+	
+	
+}
